0e810301655a5611732aa5a662ec3ea57ab44bcb
prabinzz <prabinparajuli92@gmail.com>
2017-06-06 12:35:58 +0545

    fix

--- popup.js
+++ popup.js
@@ -2,7 +2,7 @@
 var loading = true;
 var fromLang = "eng";
 var dstLang = "eng";
-localStorage.limit = 3;
+localStorage.limit = 5;
 
 var default_out = "<p class=\"tips mr-side-m\">Tip: This is a work in progress chrome extenction. For more detail visit our <a href=\"http://www.github.com/prabinzz/\">repo</a> on GitHub.</p>"
 var btn = document.getElementById("search");
@@ -19,7 +19,7 @@ function getMeaning(w){
 			if (this.readyState == 4 && this.status == 200) {
        			// Typical action to be performed when the document is ready:
        			data = JSON.parse(xreq.responseText);
-            if(data.result == "ok"){
+            if(data.result == "ok" && data.hasOwnProperty("tuc")){
               dataParse(data);
             }else{
               dataError(data);
@@ -42,13 +42,18 @@ function onLoading(){
 
 function dataParse(data){
   var temp = '';
-
+    console.log(data);
+    temp+= "<h3>"+data.phrase+"</h3>";
+    temp+="<ol>";
     for (var i = 0; i < data.tuc[0].meanings.length && i< localStorage.limit; i++) {
       temp+="<li>"+data.tuc[0].meanings[i].text+"</li>";
     }
+    temp+="</ol>";
     output.innerHTML = temp;
 }
 
-function dataError(){
-
+function dataError(data){
+  var temp = "";
+  temp += "<h4 class='center'> No match found for \""+data.phrase+"\"</h4>";
+  output.innerHTML = temp;
 }
